ffe707bca3a2d7c232c44214181267efc37c6b94
RomanKhrapai <hrapayr@gmail.com>
2022-05-09 10:35:43 +0300

    +

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/repo-name">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
